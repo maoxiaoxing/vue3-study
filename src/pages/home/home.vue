7e00cf8183b2a20ef2bf4b8c82e37b25a7dfd347
maoxiaoxing <930649054@qq.com>
2021-04-27 23:08:53 +0800

    引入sass

---
+++ src/pages/home/home.vue
@@ -0,0 +1,22 @@
+<template>
+  {{ state.msg }}
+</template>
+
+<script>
+import { reactive } from "vue";
+
+export default {
+  setup() {
+    const state = reactive({
+      msg: 'home'
+    })
+    return {
+      state,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
