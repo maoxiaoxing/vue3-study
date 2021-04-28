601983e826c1dcd651a5c91b2c27a56dfe0246a4
maoxiaoxing <930649054@qq.com>
2021-04-28 23:11:56 +0800

    menu

---
+++ src/components/Menu/Menu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <el-menu 
+      default-active=""
+    >
+      
+    </el-menu>
+  </div>
+</template>
+
+<script>
+import SubMenu from '@/components/Menu/SubMenu';
+
+export default {
+  components: {
+    SubMenu,
+  },
+  setup() {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Menu/SubMenu.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <el-submenu 
+      :title="menu.title"
+      :index="menu.id"
+    >
+      <template
+        v-for="subItem in menu.children"
+      >
+        <sub-menus
+          v-if="subItem.children && subItem.children.length > 0"
+          :menu="subItem"
+          :key="subItem.id" 
+        />
+
+        <el-menu-item
+          v-else
+          :index="subItem.id"
+          :key="subItem.id"
+        >
+          {{ subItem.title }}
+        </el-menu-item>
+      </template>
+    </el-submenu>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SubMenus',
+  props: {
+    menu: {
+      require: true,
+      default() {
+        return {
+          title: '默认导航',
+          id: Math.floor(Math.random() * 10000),
+          icon: 'user',
+          children: [
+            {
+              title: '默认子导航',
+              id: Math.floor(Math.random() * 10000)
+            }
+          ]
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
