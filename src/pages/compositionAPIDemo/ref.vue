052281255fc9a5bcd6f40ae181e0c8bb915f5304
maoxiaoxing <930649054@qq.com>
2021-05-02 10:54:37 +0800

    ref

--- src/pages/compositionAPIDemo/ref.vue
+++ src/pages/compositionAPIDemo/ref.vue
@@ -1,16 +1,35 @@
 <template>
   <div>
-    ref
+    <el-button 
+      type="primary"
+      @click="increase"
+    >加一个</el-button>
+    <p>count：{{ count }}</p>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Ref',
-  setup() {
+import { defineComponent, ref } from 'vue'
 
+const useCount = () => {
+  const count = ref(0)
+  const increase = () => {
+    console.log(123)
+    count.value++
+  }
+  return {
+    count,
+    increase,
   }
 }
+const refDemo = defineComponent({
+  setup() {
+    return {
+      ...useCount()
+    }
+  }
+})
+export default refDemo
 </script>
 
 <style scoped>
