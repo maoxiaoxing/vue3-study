208b914dbf55ec810d7aca905ae4f66993f81765
maoxiaoxing <930649054@qq.com>
2021-05-02 10:40:20 +0800

    坐标

--- src/pages/compositionAPIDemo/setup.vue
+++ src/pages/compositionAPIDemo/setup.vue
@@ -1,13 +1,43 @@
 <template>
   <div>
-    setup
+    鼠标横坐标：{{ x }}
+    鼠标纵坐标：{{ y }}
   </div>
 </template>
 
 <script>
+import { onMounted, onUnmounted, reactive, toRefs } from 'vue'
+
+const usePosition = () => {
+  const position = reactive({
+    x: 0,
+    y: 0,
+  })
+
+  const getPosition = (e) => {
+    position.x = e.pageX
+    position.y = e.pageY
+  }
+
+  onMounted(() => {
+    window.addEventListener('mousemove', getPosition)
+  })
+
+  onUnmounted(() => {
+    window.removeEventListener('mousemove', getPosition)
+  })
+
+  return toRefs(position)
+}
+
 export default {
   setup() {
+    const { x, y } = usePosition()
 
+    return {
+      x,
+      y,
+    }
   }
 }
 </script>
