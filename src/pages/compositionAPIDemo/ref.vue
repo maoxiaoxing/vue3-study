dbba0900d53e19f6c083dfae00dc829da2016cb8
maoxiaoxing <930649054@qq.com>
2021-05-07 22:52:14 +0800

    unref

--- src/pages/compositionAPIDemo/ref.vue
+++ src/pages/compositionAPIDemo/ref.vue
@@ -5,26 +5,36 @@
       @click="increase"
     >加一个</el-button>
     <p>count：{{ count }}</p>
+    <p>uncount: {{ uncount }}</p>
   </div>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, unref, toRef } from 'vue'
 
 const useCount = () => {
   const count = ref(0)
+  let uncount = unref(count)
   const increase = () => {
     count.value++
+    uncount = unref(count)
   }
   return {
     count,
     increase,
+    uncount,
   }
+}
+
+const useFoo = () => {
+  
 }
 const refDemo = defineComponent({
   setup() {
+
     return {
-      ...useCount()
+      ...useCount(),
+      ...useFoo(),
     }
   }
 })
