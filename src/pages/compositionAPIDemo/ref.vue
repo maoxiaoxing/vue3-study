8238659b751fc4ad5298533bb6db15a02c725816
maoxiaoxing <930649054@qq.com>
2021-05-08 22:39:18 +0800

    toref

--- src/pages/compositionAPIDemo/ref.vue
+++ src/pages/compositionAPIDemo/ref.vue
@@ -1,16 +1,30 @@
 <template>
   <div>
+    <p>
+      ----- ref unref -----
+    </p>
     <el-button 
       type="primary"
       @click="increase"
     >加一个</el-button>
     <p>count：{{ count }}</p>
     <p>uncount: {{ uncount }}</p>
+
+  <p>
+    ----- toref -----
+  </p>
+    <el-button 
+      type="primary"
+      @click="toggleFoo"
+    >切换foo</el-button>
+    <p>fooToRef：{{ fooToRef }}</p>
+    <p>foo: {{ fooState.foo }}</p>
+    <p>fooRef: {{ fooRef.foo }}</p>
   </div>
 </template>
 
 <script>
-import { defineComponent, ref, unref, toRef } from 'vue'
+import { defineComponent, reactive, ref, unref, toRef, toRefs } from 'vue'
 
 const useCount = () => {
   const count = ref(0)
@@ -27,8 +41,30 @@ const useCount = () => {
 }
 
 const useFoo = () => {
-  
+  // const fooState = reactive({
+  //   foo: true,
+  // })
+  const fooState = {
+    foo: true
+  }
+  const fooRef = ref(fooState)
+  let fooToRef = toRef(fooState, 'foo')
+  console.log(fooRef)
+
+  const toggleFoo = () => {
+    console.log(fooState.foo)
+    fooRef.value = !fooRef.value
+    fooToRef.value = !fooToRef.value
+  }
+
+  return {
+    fooToRef,
+    fooState,
+    toggleFoo,
+    fooRef,
+  }
 }
+
 const refDemo = defineComponent({
   setup() {
 
