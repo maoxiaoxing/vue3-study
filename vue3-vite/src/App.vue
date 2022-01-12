b6410d211972e0d6b88a9bcfdd9209ca31b269fb
maoxiaoxing <930649054@qq.com>
2022-01-12 09:44:34 +0800

    组件使用

--- vue3-vite/src/App.vue
+++ vue3-vite/src/App.vue
@@ -1,16 +1,26 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-// import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <img
     alt="Vue logo"
     src="./assets/logo.png"
   >
-  <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
+  <HelloWorld
+    ref="helloWorld"
+    msg="Hello Vue 3 + TypeScript + Vite"
+  />
 </template>
+
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { ref, onMounted } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+const helloWorld = ref<InstanceType<typeof HelloWorld>>(null)
+
+onMounted(() => {
+  console.log(helloWorld.value)
+})
+</script>
 
 <style>
 #app {
