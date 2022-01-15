bbf3349d79f0011806e9a899ace012efeeb1f5f4
maoxiaoxing <930649054@qq.com>
2022-01-15 11:42:33 +0800

    路径别名

--- vue3-vite/src/views/home/index.vue
+++ vue3-vite/src/views/home/index.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script lang="ts" setup>
-  import { useStore } from '../../store'
+  import { useStore } from '@/store'
 
   const store = useStore()
   console.log(store.state.count)
--- vue3-vite/vite.config.ts
+++ vue3-vite/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,10 @@ export default defineConfig({
     vueJsx({
       
     }),
-  ]
+  ],
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, 'src')
+    }
+  }
 })
