ea6813c071712d708b324f240d1cfe19e35eed69
maoxiaoxing <930649054@qq.com>
2022-02-15 08:09:16 +0800

    创建服务器

--- vite-principle/index.js
+++ vite-principle/index.js
@@ -1,1 +1,16 @@
 #!/usr/bin/env node
+const koa = require('koa')
+const send = require('koa-send')
+
+const app = new koa()
+
+app.use(async (ctx, next) => {
+  await send(ctx, ctx.path, {
+    root: process.cwd(),
+    index: 'index.html',
+  })
+  await next()
+})
+
+app.listen(3000)
+console.log('server is running http://localhost:3000')
