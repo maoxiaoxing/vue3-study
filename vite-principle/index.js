e2131b4853d11a557646819581d13688601010ae
maoxiaoxing <930649054@qq.com>
2022-02-16 08:16:12 +0800

    替换引用路径

--- vite-principle/index.js
+++ vite-principle/index.js
@@ -4,12 +4,28 @@ const send = require('koa-send')
 
 const app = new koa()
 
+const streamToString = (stream) => {
+  return new Promise((resolve, reject) => {
+    const chunks = []
+    stream.on('data', chunk => chunks.push(chunk))
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf-8')))
+    stream.on('error', reject)
+  })
+}
+
 app.use(async (ctx, next) => {
   await send(ctx, ctx.path, {
     root: process.cwd(),
     index: 'index.html',
   })
   await next()
+})
+
+app.use(async (ctx, next) => {
+  if (ctx.type === 'application/javascript') {
+    const contents = await streamToString(ctx.body)
+    ctx.body = contents.replace(/(from\s+['"])(?![\.\/])/g, '$1/@modules/')
+  }
 })
 
 app.listen(3000)
