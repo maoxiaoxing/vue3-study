807cdf95e995e59d73cd5c75fa9dc5979d074d42
maoxiaoxing <930649054@qq.com>
2022-03-05 10:12:25 +0800

    响应式

---
+++ vue.js设计与实现/第4章 响应式系统的作用与实现/reactivity/index.js
@@ -0,0 +1,124 @@
+const isObject = val => val !== null && typeof val === 'object'
+const convert = target => isObject(target) ? reactive : target
+const hasOwnProperty = Object.prototype.hasOwnProperty
+const hasOwn = (target, key) => hasOwnProperty.call(target, key)
+
+export function reactive (target) {
+  if (!isObject(target)) {
+    return target
+  }
+
+  const handler = {
+    get (target, key, receiver) {
+      const result = Reflect.get(target, key, receiver)
+      track(target, key)
+      return convert(result)
+    },
+    set (target, key, value, receiver) {
+      const oldValue = Reflect.get(target, key, receiver)
+      let result = true
+      if (oldValue !== value) {
+        result = Reflect.set(target, key, value, receiver)
+        trigger(target, key)
+      }
+      return true
+    },
+    deleteProperty (target, key) {
+      const hadKey = hasOwn(target, key)
+      const result = Reflect.deleteProperty(target, key)
+      if (hadKey && result) {
+        // console.log('del', key)
+        trigger(target, key)
+      }
+      return result
+    }
+  }
+
+  return new Proxy(target, handler)
+}
+
+let activeEffect = null
+export function effect (callback) {
+  activeEffect = callback
+  callback() // 访问响应式对象属性，收集依赖
+  activeEffect = null
+}
+
+let targetMap = new WeakMap()
+
+export function track (target, key) {
+  if (!activeEffect) return
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()))
+  }
+  let dep = depsMap.get(key)
+  if (!dep) {
+    depsMap.set(key, (dep = new Set()))
+  }
+  dep.add(activeEffect)
+}
+
+export function trigger (target, key) {
+  const depsMap = targetMap.get(target)
+  if (!depsMap) return
+  const dep = depsMap.get(key)
+  if (dep) {
+    dep.forEach((effect) => {
+      effect()
+    })
+  }
+}
+
+// export function ref (raw) {
+//   if (isObject(raw) && raw._v_isRef) {
+//     return
+//   }
+
+//   let value = convert(raw)
+//   const r = {
+//     _v_isRef: true,
+//     get value () {
+//       track(r, 'value')
+//       return value
+//     },
+//     set value (newValue) {
+//       if (newValue !== value) {
+//         raw = newValue
+//         value = convert(raw)
+//         trigger(r, 'value')
+//       }
+//     }
+//   }
+//   return r
+// }
+
+// export function toRefs (proxy) {
+//   const ret = proxy instanceof Array ? new Array(proxy.length) : {}
+
+//   for (const key in proxy) {
+//     ret[key] = toProxyRef(proxy, key)
+//   }
+//   return ret
+// }
+
+// function toProxyRef (proxy, key) {
+//   const r = {
+//     _v_isRef: true,
+//     get value () {
+//       return proxy[key]
+//     },
+//     set value (newValue) {
+//       proxy[key] = newValue
+//     }
+//   }
+//   return r
+// }
+
+// export function computed (getter) {
+//   const result = ref()
+//   effect(() => {
+//     result.value = getter()
+//   })
+//   return result
+// }
