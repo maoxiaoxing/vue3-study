4215a0c90625e52f38de9139208336e19cae65ec
maoxiaoxing <930649054@qq.com>
2022-03-17 21:48:50 +0800

    挂载子元素和属性

---
+++ vue.js设计与实现/第8章 挂载与更新/compiler/index.js
@@ -0,0 +1,72 @@
+
+function createRenderer(options) {
+
+  const {
+    createElement,
+    insert,
+    setElementText,
+  } = options
+
+  function render(vnode, container) {
+    if (vnode) {
+      patch(container._vnode, vnode, container)
+    } else {
+      if (container._vnode) {
+        container.innerHTML = ''
+      }
+    }
+    container._vnode = vnode
+  }
+
+  function hydrate(vnode, container) {
+
+  }
+
+  function patch(n1, n2, container) {
+    if (!n1) {
+      mountElement(n2, container)
+    } else {
+  
+    }
+  }
+
+  function mountElement(vnode, container) {
+    const el = createElement(vnode.type)
+    if (typeof vnode.children === 'string') {
+      setElementText(el, vnode.children)
+    } else if (Array.isArray(vnode.children)) {
+      vnode.children.forEach((child) => {
+        patch(null, child, el)
+      })
+    }
+
+    if (vnode.props) {
+      console.log(el, '1')
+      for (const key in vnode.props) {
+        el[key] = vnode.props[key]
+      }
+    }
+    console.log(container, el, 'ff')
+    insert(el, container)
+  }
+
+  return {
+    render,
+    hydrate,
+  }
+}
+
+export const renderer = createRenderer({
+  createElement(tag) {
+    return {
+      tag,
+    }
+  },
+  setElementText(el, text) {
+    el.text = text
+  },
+  insert(el, parent, anchor = null) {
+    parent.children = el
+    console.log(el, parent, 'pa')
+  }
+})
