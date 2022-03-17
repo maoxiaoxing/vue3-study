b701e2a4702fde6991057f71c384922f509fa18a
maoxiaoxing <930649054@qq.com>
2022-03-17 21:35:31 +0800

    render

--- vue.js设计与实现/第7章 渲染器的设计/compiler/index.js
+++ vue.js设计与实现/第7章 渲染器的设计/compiler/index.js
@@ -1,5 +1,44 @@
 
-function createRenderer() {
+function createRenderer(options) {
+
+  const {
+    createElement,
+    insert,
+    setElementText,
+  } = options
+
+  function render(vnode, container) {
+    if (vnode) {
+      patch(container._vnode, vnode, container)
+    } else {
+      if (container._vnode) {
+        container.innerHTML = ''
+      }
+    }
+    container._vnode = vnode
+  }
+
+  function hydrate(vnode, container) {
+
+  }
+
+  function patch(n1, n2, container) {
+    if (!n1) {
+      mountElement(n2, container)
+    } else {
+  
+    }
+  }
+
+  function mountElement(vnode, container) {
+    const el = document.createElement(vnode.type)
+    if (typeof vnode.children === 'string') {
+      setElementText(el, vnode.children)
+    }
+    // container.appendChild(el)
+    console.log(container, el, 'ff')
+    insert(el, container)
+  }
 
   return {
     render,
@@ -7,23 +46,17 @@ function createRenderer() {
   }
 }
 
-function render(vnode, container) {
-  if (vnode) {
-    patch(container._vnode, vnode, container)
-  } else {
-    if (container._vnode) {
-      container.innerHTML = ''
+export const renderer = createRenderer({
+  createElement(tag) {
+    return {
+      tag,
     }
+  },
+  setElementText(el, text) {
+    el.text = text
+  },
+  insert(el, parent, anchor = null) {
+    parent.children = el
+    console.log(el, parent, 'pa')
   }
-  container._vnode = vnode
-}
-
-function hydrate(vnode, container) {
-
-}
-
-function patch(n1, n2, container) {
-
-}
-
-export const renderer = createRenderer()
+})
