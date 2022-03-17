54e02329269ad80b2055aaa6c599fe94c24b1d46
maoxiaoxing <930649054@qq.com>
2022-03-17 08:29:17 +0800

    renderer

--- vue.js设计与实现/第7章 渲染器的设计/compiler/index.js
+++ vue.js设计与实现/第7章 渲染器的设计/compiler/index.js
@@ -1,5 +1,29 @@
 
-export function renderer(domString, container) {
-  console.log(domString, container)
-  container.innerHTML = domString
+function createRenderer() {
+
+  return {
+    render,
+    hydrate,
+  }
 }
+
+function render(vnode, container) {
+  if (vnode) {
+    patch(container._vnode, vnode, container)
+  } else {
+    if (container._vnode) {
+      container.innerHTML = ''
+    }
+  }
+  container._vnode = vnode
+}
+
+function hydrate(vnode, container) {
+
+}
+
+function patch(n1, n2, container) {
+
+}
+
+export const renderer = createRenderer()
