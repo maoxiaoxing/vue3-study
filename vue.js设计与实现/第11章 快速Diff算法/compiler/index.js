0703658a0a019cf252a9c106152e651ae3943d65
maoxiaoxing <930649054@qq.com>
2022-03-29 07:52:38 +0800

    getSequence

--- vue.js设计与实现/第11章 快速Diff算法/compiler/index.js
+++ vue.js设计与实现/第11章 快速Diff算法/compiler/index.js
@@ -212,6 +212,7 @@ function createRenderer(options) {
         patch(null, newChildren[j++], container, anchor)
       }
     } else if (j > newEnd && j <= oldEnd) {
+      // j 到 oldEnd 之间的元素需要卸载
       while (j <= oldEnd) {
         unmount(oldChildren[j++])
       }
@@ -251,7 +252,7 @@ function createRenderer(options) {
       }
 
       if (moved) {
-        
+
       }
     }
 
@@ -320,6 +321,47 @@ function shouldSetAsProps(el, key, value) {
     return false
   }
   return key in el
+}
+
+function getSequence(arr) {
+  const p = arr.slice()
+  const result = [0]
+  let i,j,u,v,c
+  const len = arr.length
+  for (i = i; i < len; i++) {
+    const arrI = arr[i]
+    if (arrI !== 0) {
+      j = result[result.length - 1]
+      if (arr[j] < arrI) {
+        p[i] = j
+        result.push(i)
+        continue
+      }
+      u = 0
+      v = result.length - 1
+      while (u < v) {
+        c = ((u+v)/2) | 0
+        if (arr[result[c]] < arrI) {
+          u = c + 1
+        } else {
+          v = c
+        }
+      }
+      if (arrI < arr[result[u]]) {
+        if (u > 0) {
+          p[i] = result[u - 1]
+        }
+        result[u] = i
+      }
+    }
+  }
+  u = result.length
+  v = result[u - 1]
+  while(u-- > 0) {
+    result[u] = v
+    v = p[v]
+  }
+  return result
 }
 
 export const renderer = createRenderer({
