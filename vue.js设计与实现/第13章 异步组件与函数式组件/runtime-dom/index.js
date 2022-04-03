70a9ef4b242880efaa892a2152e2b7518a9cffbe
maoxiaoxing <930649054@qq.com>
2022-04-03 21:48:12 +0800

    超时与异步组件

--- vue.js设计与实现/第13章 异步组件与函数式组件/runtime-dom/index.js
+++ vue.js设计与实现/第13章 异步组件与函数式组件/runtime-dom/index.js
@@ -1,4 +1,4 @@
-import { ref } from '../reactivity/reactivity.js'
+import { ref, shallowRef } from '../reactivity/reactivity.js'
 import { onMounted } from '../runtime-core/index.js'
 // 定义异步组件
 export function defineAsyncComponent (options) {
@@ -14,16 +14,19 @@ export function defineAsyncComponent (options) {
     name: 'AsyncComponentWrapper',
     setup() {
       const loaded = ref(false)
-      const timeout = ref(false)
+      const error = shallowRef(null)
 
-      loader().then((c) => {
-        InnerComp = c
-        loaded.value = true
-      })
+      loader()
+        .then((c) => {
+          InnerComp = c
+          loaded.value = true
+        })
+        .catch((err) => error.value = err)
       let timer = null
       if (options.timeout) {
         timer = setTimeout(() => {
-          timeout.value = true
+          const err = new Error(`Async component timed out after ${options.timeout}ms`)
+          error.value = err
         }, options.timeout)
       }
 
@@ -33,10 +36,11 @@ export function defineAsyncComponent (options) {
       return () => {
         if (loaded.value) {
           return { type: InnerComp }
-        } else if (timeout.value) {
-          return options.errorComponent ? { type: options.errorComponent } : placeholder
+        } else if (error.value && options.errorComponent) {
+          return { type: options.errorComponent, props: { error: error.value } }
+        } else {
+          return placeholder
         }
-        return placeholder
       }
     },
   }
