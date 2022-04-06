5fba77f030ea2ce0c311b8eda5178bfb4fbed9a1
maoxiaoxing <930649054@qq.com>
2022-04-06 08:09:09 +0800

    KeepAlive组件

--- vue.js设计与实现/第14章 内建组件和模块/runtime-core/index.js
+++ vue.js设计与实现/第14章 内建组件和模块/runtime-core/index.js
@@ -14,3 +14,37 @@ export function onMounted (fn) {
   }
 }
 
+export const KeepAlive = {
+  _isKeepAlive: true,
+  setup(props, { slots }) {
+    const cache = new Map()
+    const instance = currentInstance
+    const { move, createElement } = instance.KeepAliveCtx
+    const storageContainer = createElement('div')
+
+    instance._deActivate = (vnode) => {
+      move(vnode, storageContainer)
+    }
+    instance._activate = (vnode, container, anchor) => {
+      move(vnode, container, anchor)
+    }
+
+    return () => {
+      let rawVNode = slots.default()
+      if (typeof rawVNode.type !== 'object') {
+        return rawVNode
+      }
+      const cacheVNode = cache.get(rawVNode.type)
+      if (cacheVNode) {
+        rawVNode.component = cacheVNode.component
+        rawVNode.keptAlive = true
+      } else {
+        cache.set(rawVNode.type, rawVNode)
+      }
+      rawVNode.shouldKeepAlive = true
+      rawVNode.KeepAliveInstance = instance
+      return rawVNode
+    }
+  }
+}
+
