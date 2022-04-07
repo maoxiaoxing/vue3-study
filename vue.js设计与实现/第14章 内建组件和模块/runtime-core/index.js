08c9e931fbb44b16cd866113a4001de0e0561620
maoxiaoxing <930649054@qq.com>
2022-04-07 08:12:07 +0800

    添加注释

--- vue.js设计与实现/第14章 内建组件和模块/runtime-core/index.js
+++ vue.js设计与实现/第14章 内建组件和模块/runtime-core/index.js
@@ -20,6 +20,8 @@ export const KeepAlive = {
     const cache = new Map()
     const instance = currentInstance
     const { move, createElement } = instance.KeepAliveCtx
+
+    // 创建隐藏容器
     const storageContainer = createElement('div')
 
     instance._deActivate = (vnode) => {
@@ -31,17 +33,23 @@ export const KeepAlive = {
 
     return () => {
       let rawVNode = slots.default()
+      // 如果不是组件，直接渲染，因为非组件无法被 keepAlive
       if (typeof rawVNode.type !== 'object') {
         return rawVNode
       }
       const cacheVNode = cache.get(rawVNode.type)
+      // 挂载先获取缓存组件，如果有，证明组件只需要被激活，而不是挂载
       if (cacheVNode) {
         rawVNode.component = cacheVNode.component
+        // 在组件上标记 keptAlive，避免渲染器重新挂载它
         rawVNode.keptAlive = true
       } else {
+        // 添加缓存，下次激活组件就不需要挂载
         cache.set(rawVNode.type, rawVNode)
       }
+      // 标记 shouldKeepAlive 属性，避免卸载组件
       rawVNode.shouldKeepAlive = true
+      // 将实例添加到 vnode 上，以便在渲染器中访问
       rawVNode.KeepAliveInstance = instance
       return rawVNode
     }
