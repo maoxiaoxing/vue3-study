215b7790d05894e93b0bdfb092af2dc9d9824caf
maoxiaoxing <930649054@qq.com>
2022-04-19 08:21:38 +0800

    transformText

--- vue.js设计与实现/第15章 编译器核心技术概览/compiler-core/index.js
+++ vue.js设计与实现/第15章 编译器核心技术概览/compiler-core/index.js
@@ -233,10 +233,14 @@ function transformElement (node, context) {
 
 // 转换文本
 function transformText (node, context) {
-  if (node.type === 'Text') {
-    // node.content = node.content.repeat(2)
-    context.removeNode()
+  if (node.type !== 'Text') {
+    return
   }
+
+  // 文本节点对应的 JavaScript AST 节点就是一个字符串字面量
+  // 因此只需要使用 node.content 创建一个 StringLiteral 类型的节点即可
+  // 最后将文本节点对应的 JavaScript AST 节点添加到 node.jsNode 属性下
+  node.jsNode = createStringLiteral(node.content)
 }
 
 // 创建 StringLiteral 节点
