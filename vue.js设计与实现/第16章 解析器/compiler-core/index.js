74dac4f6b8e948c0864dc89566232e737975d81f
maoxiaoxing <930649054@qq.com>
2022-05-02 20:45:45 +0800

    decodeHtml

--- vue.js设计与实现/第16章 解析器/compiler-core/index.js
+++ vue.js设计与实现/第16章 解析器/compiler-core/index.js
@@ -604,9 +604,41 @@ function decodeHtml (rawText, asAttr = false) {
           maxCRNameLength = Object.keys(namedCharacterTrferences).reduce((max, name) => Math.max(max, name.length), 0)
         }
         // 从最大长度开始对文本进行截取，并试图去引用表中找到对应的项
+        for (let length = maxCRNameLength; !value && length > 0; --length) {
+          // 截取字符 & 到最大长度之间的字符作为对应项的值
+          name = rawText.substr(1, length)
+          // 使用实体名称去索引表中查找对应项的值
+          value = (namedCharacterTrferences)[name]
+        }
+        // 如果找到了对应项的值，说明解码成功
+        if (value) {
+          // 检查实体名称的最后一个匹配字符是否是分号
+          const semi = name.endsWith(';')
+          // 如果解码的文本作为属性值，最后一个匹配的字符不是分号
+          // 并且最后一个匹配字符的下一个字符是等于号 (=) ASCII 字母或数字
+          // 由于历史原因 将字符 & 和实体名称 name 作为普通文本
+          if (asAttr && !semi && /[=a-z0-9]/i.test(rawText[name.length + 1] || '')) {
+            decodedText += '&' + name
+            advance(1 + name.length)
+          } else {
+            // 其他情况下，正常使用解码后的内容拼接到 decodedText 上
+            decodedText += value
+            advance(1 + name.length)
+          }
+        } else {
+          // 如果没有找到对应的值，说明解码失败
+          decodedText += '&' + name
+          advance(1 + name.length)
+        }
+      } else {
+        // 如果字符 & 的下一个字符不是 ASCII 字母或数字，则将字符 & 做为普通文本
+        decodedText += '&'
+        advance(1)
       }
     }
   }
+
+  return decodedText
 }
 
 // 创建 StringLiteral 节点
